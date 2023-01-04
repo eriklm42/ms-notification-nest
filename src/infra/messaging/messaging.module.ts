06de7692cd28fa573d1249f0f850dd86868280f4
user.email <eriklm42@gmail.com>
2023-01-04 10:39:25 -0300

    fix: depends modules messaging

--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,10 +1,12 @@
+import { SendNotification } from '@application/use-cases/notification/send';
+import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { KafkaConsumerService } from './kafka/consumer.service';
 import { NotificationsController } from './kafka/controllers/nofitifications.controler';
 
 @Module({
-  imports: [],
-  providers: [KafkaConsumerService],
+  imports: [DatabaseModule],
+  providers: [KafkaConsumerService, SendNotification],
   controllers: [NotificationsController],
 })
 export class MessagingModule {}
